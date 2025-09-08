5dda45d4aa7c4979dd2affcde03d2e12bde21bea
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-09-08 09:53:16 -1000

    Add CORS to allow frontend requests

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const prisma = require('./prisma');
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.get('/api', (req, res) => res.send('Hello, world!'));
 
 app.get('/api/characters', async (req, res) => {
